09ff1f3a2bfda6d4d91f3ad3a4089406bd89e66d
Kevin-M-Simiyu <kevinmukubwa61@gmail.com>
2026-05-26 21:35:36 -0700

    change

--- vignettes/HW4_simulation_plot.R
+++ vignettes/HW4_simulation_plot.R
@@ -50,7 +50,7 @@ dev.off()rm(list = ls())
 library(UWBiost561)
 
 # Load simulation results
-load("~/HW4_simulation.RData")
+load("HW4_simulation.RData")
 
 # Summarize
 summary_valid <- aggregate(valid ~ alpha + method, data = simulation_results, FUN = mean)
